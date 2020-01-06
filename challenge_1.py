620baef3d30c47883a2024f2b76f64db11cbea41
edeediong <edidiongmichael08@gmail.com>
2020-01-06 10:49:04 +0100

    docs: added documentations

--- challenge_1.py
+++ challenge_1.py
@@ -1,4 +1,5 @@
 def multiples_of_3_and_5(number):
+    """Function to calculate the sum of the multiples of 3 and 5 of a given number."""
     content = []
     for i in range(1, number):
         if i%3 == 0 or i%5 == 0:
